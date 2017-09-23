60ba6a202ac7643443abda67f46ee737f6f98c8a
NareSed95 <nares_sedrakyan@edu.aua.am>
2017-09-23 18:08:27 +0400

    secound commit

---
+++ Desktop/HW3/homework3.js
@@ -0,0 +1,62 @@
+const multiple = function(x, y, z) {
+	return x * y * z;
+};
+
+
+
+ const greetToMyUniversityoMyUniversity = function() {
+	return 'HelloAUA';
+};
+
+
+const b = function(string1, string2) {
+	console.log(string1+" "+string2);
+};
+
+
+
+const fullName = function(FirstName, LastName) {
+	return FirstName +" "+ LastName;
+};
+
+
+const x = function(string1, string2, string3) {
+	if (string1.length > string2.length && string1.length > string3.length)
+	return string1; 
+	if (string2.length > string1.length && string2.length > string3.length)
+	return string2;
+	if (string3.length > string1.length && string3.length > string2.length)
+	return string3;
+};
+
+
+
+
+
+
+const numbers = function(number1, number2) {
+	if (number1 === number2) {
+		return	0;
+	}
+	
+	if (number1 > number2) {
+		return 1;
+	}
+	
+	else {
+		return -1;
+	}
+};
+
+const isTruethy = function(arg1, arg2, arg3) {
+	if (arg1) {
+return arg1;
+	}
+if (arg2) {
+	return arg2;
+}
+if (arg3) {
+	return arg3;
+}
+};
+	
